3df15811bcc5ab0d923d4929db283876d85ba2e2
Andrei Mondoc <andreimondoc@gmail.com>
2019-03-14 02:04:43 +0000

    add Endpoint_Interface

---
+++ src/Endpoint/Endpoint-Interface.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Endpoint Interface class.
+ *
+ * @since 0.1
+ */
+
+namespace CiviCRM_WP_REST\Endpoint;
+
+interface Endpoint_Interface {
+
+	/**
+	 * Route namespace.
+	 *
+	 * @since 0.1
+	 * @var string
+	 */
+	const NAMESPACE = 'civicrm/v3';
+
+	/**
+	 * Registers routes.
+	 *
+	 * @since 0.1
+	 */
+	public function register_routes();
+	
+	/**
+	 * Item schema.
+	 *
+	 * @since 0.1
+	 * @return array $schema
+	 */
+	public function get_item_schema();
+
+	/**
+	 * Item arguments.
+	 *
+	 * @since 0.1
+	 * @return array $arguments
+	 */
+	public function get_item_args();
+
+}
